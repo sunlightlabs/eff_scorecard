7875553cdb787fa4cfaa8b35406b8ac5d3cdb0c1
sbma44 <thomas.j.lee@gmail.com>
2014-06-15 19:54:59 -0400

    working first pass

--- scorecard.py
+++ scorecard.py
@@ -1,23 +1,33 @@
-# import sunlight
+import unicodecsv
+import argparse
 from sunlight import congress
-# from sunlight.pagination import PagingService
 
-def main():
+def main(outfile):
 
     representatives = []
     senators = []   
     scores = {}
+
+    legislator_metadata = {}
+
+    fields_to_collect = ['title','first_name','last_name','party','state','district','twitter_id','youtube_id','facebook_id']
 
     # grab bioguides
     print 'Collecting legislators...'
     for (varname, chamber) in ((senators, 'senate'), (representatives, 'house')):
         i = 0
         while True:
-            new_reps = congress.legislators(chamber=chamber, fields='bioguide_id', page=i)
+            new_reps = congress.legislators(chamber=chamber, fields=','.join(['bioguide_id',] + fields_to_collect), page=i)
             if new_reps is None:
                 break
             for nr in new_reps:
-                varname.append(nr.get('bioguide_id'))
+                bioguide = nr.get('bioguide_id')
+                varname.append(bioguide)
+
+                legislator_metadata[bioguide] = {}
+                for k in fields_to_collect:
+                    legislator_metadata[bioguide][k] = nr.get(k)
+
             i = i + 1
     
     # initialize scores dict
@@ -53,8 +63,8 @@ def main():
     print 'checking votes on House version of USA FREEDOM...'
     votes = congress.votes(bill_id='hr3361-113', fields='voters')
     for legislator in scores:
-        score = 0
-        if votes[0]['voters'][legislator]['vote'] == 'Aye':
+        score = 0        
+        if votes[0]['voters'].get('legislator', {}).get('vote') == 'Aye':
             score = -3
         scores[legislator]['voted for House version of USA Freedom (-3)'] = score
 
@@ -72,12 +82,82 @@ def main():
 
 
     # cosponsored House version of USA Freedom on or before 5/18/2014 (+3)
-    # Conyers-Amash Amendment (vote +4)
-    # Rogers Bill (cosponsor -4)
+    # TODO: resolve bug related to sponsorships field not coming back
+    # sponsorships = congress.bills(bill_id='hr3361-113', fields='sponsorships')
+
+
+    # Conyers-Amash Amendment amendment: [hamdt413-113] vote: [h412-2013] (vote +4)
+    print 'checking votes on Conyers/Amash amendment...'
+    votes = congress.votes(roll_id='h412-2013', fields='voters')
+    for legislator in scores:
+        score = 0
+        if votes[0]['voters'].get('legislator', {}).get('vote') == 'Aye':
+            score = 4
+        scores[legislator]['voted for Conyers/Amash amendment (+4)'] = score
+
+
+    # sponsor/cosponsor of FISA Transparency & Modernization Act (Rogers/MI) [hr4291-113] (cosponsor -4)
+    print 'checking sponsor/cosponsors of FISA Transparency & Modernization Act (Rogers bill)...'
+    bill = congress.bills(bill_id='hr4291-113', fields='cosponsor_ids,sponsor_id')[0]
+    sponsor_and_cosponsors_ids = bill.get('cosponsor_ids') + [bill.get('sponsor_id')]
+    
+    for legislator in scores:
+        score = 0
+        if legislator in sponsor_and_cosponsors_ids:
+            score = -4
+        scores[legislator]['sponsor/cosponsor of FISA Transparency & Modernization Act (-4)'] = score
+
+
+
     # Reauthorizing Section 215 of the Patriot Act (vote(?) -.5)
-    # Reauthorizing FISA Amendments Act (vote(?) -.5)
+    # TODO: figure out which vote(s) are relevant
     
 
+    # Voted to reauthorize FISA Amendments Act [hr5949-112] roll IDS [s236-2012, h569-2012] (vote(?) -.5)
+    print 'checking votes on reauthorization of FISA Amendments Act...'
+    for roll_id in ('s236-2012', 'h569-2012'):
+        votes = congress.votes(roll_id=roll_id, fields='voters')
+        for legislator in scores:
+            score = 0
+            if votes[0]['voters'].get('legislator', {}).get('vote') == 'Aye':
+                score = -3
+            scores[legislator]['voted for reauthorization of FISA Amendments Act (-0.5)'] = score
+
+    print 'Writing file...'
+    f = open(outfile, 'w')
+    writer = unicodecsv.writer(f)
+    
+    # figure out what we've actually recorded here
+    fields_to_emit = {}
+    for l in scores:
+        for k in scores[l]:
+            fields_to_emit[k] = True
+    fields_to_emit = sorted(fields_to_emit.keys())
+
+    writer.writerow(['bioguide_id'] + fields_to_emit + ['total'] + fields_to_collect)
+
+    # tabulate data
+    for bioguide_id in scores:
+        out_row = [bioguide_id]
+        total = 0
+        for field in fields_to_emit:
+            value = scores[bioguide_id].get(field, 0)
+            out_row.append(value)
+            total = total + value
+
+        out_row.append(total)
+        
+        for field in fields_to_collect:
+            out_row.append(legislator_metadata[bioguide_id][field])
+
+        writer.writerow(out_row)
+
+    f.close()
+
+    print 'Done.'
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Calculate legislators\'s scores based on a bunch of digital civil liberties votes/actions.')
+    parser.add_argument('outfile', metavar='F', type=str, help='filename to write output to')
+    args = parser.parse_args()    
+    main(args.outfile)
